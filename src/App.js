01d8dac2f43b950fcc8e2d939cdfc600d65ceae6
Nagesh Kharat <130240484+nageshkharat@users.noreply.github.com>
2025-02-15 23:34:19 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import { useEffect } from "react";
 import Aos from "aos";   
-import Footer from "./UI/Footer";   
+import Footer from "./UI/Footer";    
 import Header from "./component/Header";
 import AllRoutes from "./component/AllRoutes"; 
 import Chatbot from "./UI/Chatbot"; // Import Chatbot 
