83dd6a451d2a24eb59bcff3caf2f0eecb838a99e
Nagesh Kharat <130240484+nageshkharat@users.noreply.github.com>
2025-06-22 15:24:51 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 
 import App from "./App";
 import "remixicon/fonts/remixicon.css";
-import "aos/dist/aos.css"
+import "aos/dist/aos.css" 
 import { BrowserRouter } from 'react-router-dom';
 import { Auth0Provider } from "@auth0/auth0-react";
 import AuthContextProvider from "./component/AuthContextProvider";
